f304e1000ebedb8c8e526f7a80c1218589d223aa
Briquelebrit <miguelmejiajimenez@hotmail.com>
2020-07-12 20:10:34 +0200

    Added some doxygen documentation

---
+++ include/config.hpp
@@ -0,0 +1,13 @@
+#ifndef __GE_CONFIG_HPP__
+#define __GE_CONFIG_HPP__
+
+/** @mainpage Home
+ * # Introduction
+ * This is an introduction
+ * # Instalation
+ * This is the instalation section
+ * 
+ */
+
+
+#endif
